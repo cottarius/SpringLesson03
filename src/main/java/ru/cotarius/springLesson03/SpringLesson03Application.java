cf1c0f387ae28fcda2d06d6451d590bd3c631e4a
Oleg Prokopenko <cotarius@yandex.ru>
2024-03-11 00:37:41 +0300

    1

--- src/main/java/ru/cotarius/springLesson03/SpringLesson03Application.java
+++ src/main/java/ru/cotarius/springLesson03/SpringLesson03Application.java
@@ -3,6 +3,7 @@ package ru.cotarius.springLesson03;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringLesson03Application {
 
